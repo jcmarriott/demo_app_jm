21121abac7376847e8c6a8bb60e834998e06bd73
Jeff Marriott <marriott.jeff@gmail.com>
2011-11-01 17:13:21 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoAppJm::Application.config.secret_token = '3a66a140fc72bb0e92d18f03a2a23f25df76466ba2124fd6cbece0c28bbfa4ea17e2880c76e8837fdd0108d7541d3d1057635a2805bcca59558947019f744137'
